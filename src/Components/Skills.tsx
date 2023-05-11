8cc062df002eaa0a78540ec2975c6ece3897b637
UlissesOrtega <51983586+Ulissesortega@users.noreply.github.com>
2023-05-11 10:50:17 -0700

    update

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -5,6 +5,8 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 export default function Skills() {
   return (
-    <div className='bg3'>Skills Component</div>
+    <div className='bg3'>
+      <p>Skills Component</p>
+    </div>
   )
 }
