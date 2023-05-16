fd83760d45b029c4e950e76050987286a18b19f7
Ulissesortega <Ulisses.ortega@hotmail.com>
2023-05-16 01:17:50 -0700

    update

--- src/Components/MyNavbar.tsx
+++ src/Components/MyNavbar.tsx
@@ -1,11 +1,13 @@
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 export default function MyNavBar() {
   return (
     <Container>
-    <Navbar className='fixed-top' bg="primary" variant="dark">
+    {/* <Navbar className='fixed-top' bg="primary" variant="dark">
         <Nav className="mx-auto">
           <a className="text-white LinkSpace" href="#" >HOME</a>
           <a className="text-white LinkSpace" href="#About" >ABOUT</a>
@@ -13,7 +15,27 @@ export default function MyNavBar() {
           <a className="text-white LinkSpace" href="#Project" >PROJECTS</a>
           <a className="text-white LinkSpace" href="#Contact" >CONTACT</a>
         </Nav>
+    </Navbar> */}
+     <Navbar collapseOnSelect expand="lg" bg="primary" variant="primary" fixed="top"  >
+      <Container>
+        <Navbar.Brand href="#Main" className='d-block d-sm-none'></Navbar.Brand >
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav ">
+          <Nav className="mx-auto tf5">
+            <Nav.Link className="text-white" href="#Main">HOME</Nav.Link>
+            <Nav.Link className="text-white" href="#About">ABOUT</Nav.Link>
+            <Nav.Link className="text-white" href="#Skills">SKILLS</Nav.Link>
+            <Nav.Link className="text-white" href="#Project">PROJECTS</Nav.Link>
+            <Nav.Link className="text-white" href="#Contact">CONTACT</Nav.Link>         
+          </Nav>         
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
+
     </Container>
+    
   );
 }
+
+
+
